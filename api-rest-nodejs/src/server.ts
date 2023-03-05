b90369884195aaa7db6656ab90e33a3fd10911b7
Danubio de Araújo <danubio.bwm@gmail.com>
2023-03-05 00:19:36 -0300

    config env

--- api-rest-nodejs/src/server.ts
+++ api-rest-nodejs/src/server.ts
@@ -1,17 +1,26 @@
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'transação de teste',
+  //     amount: 100,
+  //   })
+  //   .returning('*')
+  const transaction = await knex('transactions').select('*')
 
-  return tables
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
