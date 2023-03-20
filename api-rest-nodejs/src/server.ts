a52361a06a037604e8469639dbdc3c5d17840aef
Danubio de Araújo <danubio.bwm@gmail.com>
2023-03-19 21:03:55 -0300

    types knex

--- api-rest-nodejs/src/server.ts
+++ api-rest-nodejs/src/server.ts
@@ -1,21 +1,11 @@
 import fastify from 'fastify'
-// import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'transação de teste',
-  //     amount: 100,
-  //   })
-  //   .returning('*')
-  const transaction = await knex('transactions').select('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
